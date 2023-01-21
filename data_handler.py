b168bde018fb86e2dde9fbb575dd7a50be255a9c
nine57 <gusrn015@gmail.com>
2023-01-22 00:10:17 +0900

    기본 기능 구현 완성

--- data_handler.py
+++ data_handler.py
@@ -1,59 +1,105 @@
 import csv
+import random
 
 
 class DataLoader:
-    def __init__(self, csv_path="data.csv", team_len=5):
-        self.csv_path = csv_path
-        self.team_len = team_len
-        # self.this_round = None
-        # self.last_round = None
-        # self.required_check_round = 1
-        # self.required_check_leader = 1
+    def __init__(
+        self,
+        prev_data_file_path="data.csv",
+        total_team_number=5,
+        round_number=1,
+    ):
+        self.prev_data_file_path = prev_data_file_path
+        self.total_team_number = total_team_number
+        self.round_number = round_number
 
     def load_prev_data(self):
-        prev_team = {i+1: list() for i in range(self.team_len)}
-        with open(self.csv_path, "r") as f:
-            reader = csv.reader(f)
-            titles = next(reader)
-            total_data_len = len(titles)
-            for idx, line in enumerate(reader):
-                prev_team[idx+1] = line[total_data_len-2]
-        return prev_team
+        if self.round_number == 1:
+            prev_team_list_dict = {
+                i+1: list() for i in range(self.total_team_number)
+            }
+            prev_leader_list = list()
+            with open(self.prev_data_file_path, "r") as f:
+                reader = csv.reader(f)
+                titles = next(reader)
+                total_data_len = len(titles)
+                for idx, line in enumerate(reader):
+                    prev_team_list_dict[idx + 1] = line[total_data_len - 2]
+                    prev_leader_list.append(line[total_data_len - 1])
+            return prev_team_list_dict, prev_leader_list
+        elif self.round_number == 2:
+            pass
 
 
-class Test:
-    def logic(self):
-        with open('data.csv', 'r') as f:
-            reader = csv.reader(f)
-            titles = next(reader)
-            for idx, info_set in enumerate(titles):
-                print(idx, info_set)
-                if idx == 0:
-                    continue
-                num, info = info_set.split("-")[:2]
-                if "team" in info:
-                    team = num
-                elif "round" in info:
-                    rnd = num
-            for member in reader:
-                name = member[0]
+class DataProcessor:
+    prev_leader_list = ["Angie", "Susie", "Tami", "Otis", "Ken"]
+    prev_team_list_dict = {
+        1: ["Acker", "Angie", "Ann", "Dale", "Ed"],
+        2: ["Joy", "Kevin", "Leo", "Susie"],
+        3: ["EL", "Mason", "Roxy", "Tami"],
+        4: ["Jane", "Jeus", "Lego", "Otis"],
+        5: ["Becky", "Jake", "Ken", "Matthew"]
+    }
 
-    def test(self):
-        with open('sample_data.csv', 'r') as f:
+    def __init__(
+        self,
+        prev_data_file_path=None,
+        member_data_file_path="members.csv",
+        prev_leader_list=None,
+        prev_team_list_dict=None,
+        total_team_number=5
+    ):
+        self.total_team_number = total_team_number
+        self.member_data_file_path = member_data_file_path
+        self.prev_leader_list = [
+            member.lower() for member in prev_leader_list
+        ] if not None else None
+        self.prev_team_list_dict = {
+            idx: [member.lower() for member in members]
+            for idx, members in prev_team_list_dict.items()
+        } if not None else None
+        self.team_list = dict()
+        self.leader_list = list()
+
+    def process(self):
+        for i in range(1, self.total_team_number+1):
+            self.team_list[i] = list()
+
+        team_org = list()
+        team_org_members = dict()
+        team_members = list()
+
+        with open(self.member_data_file_path, 'r') as f:
             reader = csv.reader(f)
             for line in reader:
-                print(line)
+                org_name = line[0]
+                org_members = [
+                    member.lower().strip()
+                    for member in line[1].split(",")
+                ]
+                team_org.append(org_name)
+                team_org_members[org_name] = org_members
+                team_members += org_members
 
+        group_num = random.randint(1, self.total_team_number)
+        random.shuffle(team_org)
+        for _ in range(len(team_org)):
+            org = team_org.pop()
+            members = team_org_members[org]
+            random.shuffle(members)
+            for _ in range(len(members)):
+                member = members.pop()
+                group_members = self.team_list[group_num]
+                group_members.append(member)
+                self.team_list[group_num] = group_members
+                group_num += 1
+                if group_num > self.total_team_number:
+                    group_num = 1
 
-if __name__ == '__main__':
-    app = Test()
-    mode = None
-    while mode not in ["q", "exit"]:
-        mode = input()
-        if mode in ["logic", "l", "run"]:
-            app.logic()
-        elif mode in ["test", "t"]:
-            app.test()
-        else:
-            a = DataLoader()
-            a.load_prev_data()
+        for team_num in self.team_list.keys():
+            people = self.team_list[team_num]
+            random.shuffle(people)
+            for person in people:
+                if person not in self.prev_leader_list:
+                    self.leader_list.append(person)
+                    break
--- random_lunch.py
+++ random_lunch.py
@@ -16,13 +16,18 @@ from PyQt6.QtWidgets import (
     QTableWidgetItem,
     QHeaderView,
 )
-from data_handler import DataLoader
+from data_handler import DataLoader, DataProcessor
 from error import FileTypeError
 
 
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.prev_team_list_dict = None
+        self.prev_leader_list = None
+        self.prev_data_file_path = None
+        self.round_number = 1
+        self.total_team_number = 5
         self._init_ui()
         self._init_util()
 
@@ -39,12 +44,13 @@ class Main(QMainWindow):
         self.file_path_box = QLineEdit(self)
         self.team_number_box = QSpinBox(self)
         self.team_set_button = QPushButton("Team Set", self)
-        # self.round_number_box = QSpinBox(self)
-        # self.round_set_button = QPushButton("Round Set", self)
+        self.round_number_box = QSpinBox(self)
+        self.round_set_button = QPushButton("Round Set", self)
         # self.result_box = QPlainTextEdit(self)
         self.start_button = QPushButton("Start", self)
         self.save_result_button = QPushButton("Save Result", self)
         self.show_members_button = QPushButton("Show Members", self)
+        self.clear_button = QPushButton("Clear", self)
         # self.leader_exist_check = QCheckBox(self)
         self.error_dialog = QMessageBox()
         # self.input_box = QLineEdit(self)
@@ -72,13 +78,14 @@ class Main(QMainWindow):
         grid.addWidget(self.team_number_box, 1, 0, 1, 1)
         grid.addWidget(self.team_set_button, 1, 1, 1, 1)
         # grid.addWidget(self.leader_exist_check, 2, 3, 1, 1)
-        # grid.addWidget(self.round_number_box, 2, 0, 1, 1)
-        # grid.addWidget(self.round_set_button, 2, 1, 1, 1)
+        grid.addWidget(self.round_number_box, 1, 2, 1, 1)
+        grid.addWidget(self.round_set_button, 1, 3, 1, 1)
         # Action
         # grid.addWidget(self.result_box, 3, 0, 3, 3)
         # self.result_box.setPlaceholderText("랜덤 매칭 결과")
-        grid.addWidget(self.result_table, 2, 0, 3, 4)
+        grid.addWidget(self.result_table, 2, 0, 4, 4)
         grid.addWidget(self.start_button, 2, 4, 1, 1)
+        grid.addWidget(self.clear_button, 5, 4, 1, 1)
         grid.addWidget(self.save_result_button, 3, 4, 1, 1)
 
     def _init_util(self):
@@ -88,43 +95,81 @@ class Main(QMainWindow):
         self.total_team_number = 5
         self.team_set_button.clicked.connect(self.set_total_team_number)
         self.show_members_button.clicked.connect(self.set_table_by_org_member)
-        # self.round_number_box.setValue(1)
-        # self.round_number = 1
-        # self.round_set_button.clicked.connect(self.set_round_number)
-        self.start_button.clicked.connect(self.match_team)
+        self.round_number_box.setRange(1, 2)
+        self.round_number_box.setValue(1)
+        self.round_number = 1
+        self.round_set_button.clicked.connect(self.set_round_number)
+        self.start_button.clicked.connect(self.start_team_match)
         self.save_result_button.clicked.connect(self.save_result_to_csv_file)
+        self.clear_button.clicked.connect(self.result_table.clear)
         # self.input_box.returnPressed.connect(self.set_total_team_number)
 
     def open_file(self):
         window = QFileDialog()
         filename = window.getOpenFileName()
         try:
-            self.file_path = filename[0]
-            self.file_name = self.file_path.split("/")[-1]
-            if self.file_name.split(".")[-1] != "csv":
+            self.prev_data_file_path = filename[0]
+            file_name = self.prev_data_file_path.split("/")[-1]
+            if file_name.split(".")[-1] != "csv":
                 raise FileTypeError("csv 파일을 선택해주세요")
         except IndexError as e:
             self.error_dialog.about(self, "알수 없는 에러", f"{e}")
         except FileTypeError as e:
             self.error_dialog.about(self, "파일 선택 에러", f"{e}")
         else:
-            self.file_path_box.setText(self.file_path)
-            loader = DataLoader(self.file_path)
-            print(loader.load_prev_data())
+            self.file_path_box.setText(self.prev_data_file_path)
+            loader = DataLoader(
+                prev_data_file_path=self.prev_data_file_path,
+                total_team_number=self.team_number_box.value(),
+                round_number=self.round_number_box.value(),
+            )
+            self.prev_team_list_dict, self.prev_leader_list = loader.load_prev_data()
 
     def set_total_team_number(self):
-        self.total_team_number = self.team_number_box.value()
+        if not self.prev_data_file_path:
+            return
+        if self.team_number_box.value() != self.total_team_number:
+            self.total_team_number = self.team_number_box.value()
+            loader = DataLoader(
+                prev_data_file_path=self.prev_data_file_path,
+                total_team_number=self.total_team_number,
+                round_number=self.round_number,
+            )
+            self.prev_team_list_dict, self.prev_leader_list = loader.load_prev_data()
 
     def set_round_number(self):
-        self.round_number = self.round_number_box.value()
+        if not self.prev_data_file_path:
+            return
+        if self.round_number_box.value() != self.round_number:
+            self.round_number = self.round_number_box.value()
+            loader = DataLoader(
+                prev_data_file_path=self.prev_data_file_path,
+                total_team_number=self.total_team_number,
+                round_number=self.round_number,
+            )
+            self.prev_team_list_dict, self.prev_leader_list = loader.load_prev_data()
 
-    def match_team(self):
-        print("start matching")
-        self.error_dialog.about(self, "작업 중", "미완성 영역입니다")
+    def start_team_match(self):
+        self.set_total_team_number()
+        self.set_round_number()
+        processor = DataProcessor(
+            prev_data_file_path=self.prev_data_file_path,
+            member_data_file_path="members.csv",
+            prev_leader_list=self.prev_leader_list,
+            prev_team_list_dict=self.prev_team_list_dict,
+            total_team_number=self.total_team_number,
+        )
+        processor.process()
+        self.set_table_by_processed_team(
+            team=processor.team_list, leader=processor.leader_list,
+        )
 
     def save_result_to_csv_file(self):
         print("start saving result")
         self.error_dialog.about(self, "작업 중", "미완성 영역입니다")
+
+    def set_cell_with_data(self, row, column, data):
+        return self.result_table.setItem(row, column, QTableWidgetItem(data))
 
     def set_table_by_org_member(self):
         row = 0
@@ -137,8 +182,17 @@ class Main(QMainWindow):
                 self.set_cell_with_data(row, 0, line[0])
                 self.set_cell_with_data(row, 1, line[1])
 
-    def set_cell_with_data(self, row, column, data):
-        return self.result_table.setItem(row, column, QTableWidgetItem(data))
+    def set_table_by_processed_team(self, team, leader):
+        self.result_table.setRowCount(self.total_team_number+1)
+        row = 0
+        self.set_cell_with_data(row, 0, "")
+        self.set_cell_with_data(row, 1, "조장")
+        self.set_cell_with_data(row, 2, "조원")
+        for group in range(self.total_team_number):
+            row += 1
+            self.set_cell_with_data(row, 0, f"{group + 1}조")
+            self.set_cell_with_data(row, 1, str(leader[group]))
+            self.set_cell_with_data(row, 2, str(team[group+1]))
 
 
 if __name__ == '__main__':
